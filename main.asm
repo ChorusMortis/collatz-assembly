7bf1e7cc8daf304622f8095c49e2640a50364e3e
Brett Harley Mider <114895313+ChorusMortis@users.noreply.github.com>
2024-03-09 23:37:49 +0800

    Add validation for 0, negative, and empty inputs

--- main.asm
+++ main.asm
@@ -10,17 +10,21 @@ main:
     GET_DEC 8, rax
     GET_CHAR STR_INPUT ; remove trailing line feed in input
     
+    test rax, rax
+    js negative
+    test rax, rax
+    jz invalid
+    
     PRINT_STRING "Sequence: "
     PRINT_UDEC 8, rax
     
 collatz:
     cmp rax, 1
-    jz collatz_end
+    jz prompt
     
     test rax, 1
-    jz even
-    jmp odd
-
+    jnz odd
+    
 even:
     shr rax, 1 ; rax <- rax / 2
     PRINT_STRING ", "
@@ -33,7 +37,14 @@ odd:
     PRINT_UDEC 8, rax
     jmp collatz
 
-collatz_end:
+negative:
+    PRINT_STRING "Error: Input cannot be negative"
+    jmp prompt
+
+invalid:
+    PRINT_STRING "Error: Input cannot be zero, empty, or a string"
+    
+prompt:
     NEWLINE
     PRINT_STRING "Do you want to continue (Y/N)? "
     GET_STRING STR_INPUT, 3
